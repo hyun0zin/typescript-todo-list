8bfe94f3562c1023a28db92eca4c2fbacd3161fb
HyunjinKim <koujie11@gmail.com>
2024-03-08 11:30:17 +0900

    feat : add TodoForm

--- src/components/Todos/TodoForm.tsx
+++ src/components/Todos/TodoForm.tsx
@@ -1,11 +1,31 @@
+import { Todo } from "../../types/TodoType";
+
 function TodoForm() {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const title = formData.get("title") as string;
+    const content = formData.get("content") as string;
+
+    const todo: Todo = {
+      id: crypto.randomUUID(),
+      title,
+      content,
+      isDone: false,
+      deadline: new Date().toLocaleDateString(),
+    };
+
+    console.log("todo", todo);
+  };
+
   return (
     <section>
-      <form>
+      <form onSubmit={onSubmit}>
         <input type="text" name="title" />
         <input type="text" name="content" />
         <input type="date" />
-        <button>제출</button>
+        <button type="submit">제출</button>
       </form>
     </section>
   );
